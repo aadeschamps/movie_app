67a9574efbb76026ac310862a14f09e46334c9be
Alexander Deschamps <alexadeschamps@gmail.com>
2015-01-17 21:36:17 -0500

    fixed sliders, added popups

--- public/main.js
+++ public/main.js
@@ -1,11 +1,9 @@
 
 var comedies = ["Ice Age", "She's out of my league", "Shaun of the Dead", "Robin Hood Men In Tights", "Wedding Crashers", "Dodgeball"];
 var dramas = ["The Godfather", "Forrest Gump", "12 Angry Men", "Titanic", "Fight Club", "Pulp Fiction"];
-var action = ["Saving Private Ryan" ,"Taken", "X-men", "The Expendables", "Captain America", "Die Hard", "Die Hard 2"];
+var action = ["Saving Private Ryan" ,"Taken", "X-men", "The Expendables", "Die another day", "Die Hard", "Die Hard 2"];
 var c_length = (comedies.length * 25) + "%";
-var sliders = [ $('#comedy-slider'), $('#drama-slider'), $('#action-slider')];
 
-console.log(sliders);
 $('#comedy-slider').css("width", c_length);
 for (var i = 0; i < comedies.length; i++) {
 	$('#comedy-slider').append("<div class='com_movies movies'></div>");
@@ -48,9 +46,6 @@ $('.a_img').each( function(index, item){
 	getImgUrl(action[index], item);
 });
 
-
-
-
 function getImgUrl(movie, item){
 	var urled = encodeURI(movie);
 	var url = " http://omdbapi.com/?t=" + urled;
@@ -69,54 +64,70 @@ var curr_x = [0, 0, 0];
 $(".slider").each( function(index, item){
 	$( this ).mousedown(function(event){
 		var offset = $( this ).parent().offset();
-		console.log(this.offsetLeft);
-		console.log(event.pageX - offset.left);
 		mouse[index] = true;
 		curr_x[index] = (event.pageX - offset.left);
-		console.log(curr_x);
-		// console.log(curr_x);
 	});
 
 	$( this ).mouseup(function(){
 		mouse[index] = false;
-		// var current = window.parseFloat(proj_cont.style.marginLeft.replace("%",""));
-		// if( current < -(curr_proj * 100) ){
-		// 	if( current < -(curr_proj * 100 + 25) ){
-		// 		curr_proj++;
-		// 	}
-		// 	var new_pos = -(curr_proj * 100) + "%";
-		// 	$("#proj_cont").animate({"margin-left": new_pos}, 200);	
-		// }else{
-		// 	if( current > -( (curr_proj - 1) * 100 + 75) ){
-		// 		if(curr_proj > 0){curr_proj--;}
-		// 	}
-		// 	var new_pos = -(curr_proj * 100) + "%";
-		// 	$("#proj_cont").animate({"margin-left": new_pos}, 200);
-		// }
 	});
 	$( this ).mousemove(function(e){
 		if(mouse[index]){
-			var abs_pos = e.clientX / window.innerWidth * 100;
-			var change = abs_pos - curr_x;
-			var abs_current = proj_cont.style.marginLeft.replace("%", "");
-			curr_x = e.clientX / window.innerWidth * 100;
-			var current = window.parseFloat(abs_current);
-			if (change < 0){
-				if( (-current) < proj_width - 100){
-					if(current + change <= -(proj_width - 100)){
-						console.log("hello");
-						change = -(proj_width - 100) - current;
-					}
-					move(change);
-				}	
-			}else if(change > 0){
-				if(current < 0){
-					if(current + change >= 0){
-						change = -(current);
-					}
-					move(change);
-				}
+			var offset = $( this ).parent().offset();
+			var new_pos = e.pageX - offset.left;
+			var change = new_pos - curr_x[index];
+			curr_x[index] = new_pos;
+			var strMarginLeft = $( this ).css("margin-left").replace("px","");
+			var marginLeft = window.parseInt(strMarginLeft);
+			var move = marginLeft + change;
+			if(move > 0){
+				move = 0;
 			}
+			var str_movie_width = $( this ).first().css("width").replace("px","");
+			var movie_width = window.parseInt(str_movie_width) / $( this ).children().length;
+			var left_barrier = ($( this ).children().length * movie_width) - (movie_width * 4);			if( move < -left_barrier){
+				move = -left_barrier;
+			}
+			var str_move = move + "px";
+			// console.log(str_move);
+			$( this ).css("margin-left", str_move);
 		}
 	});
+});
+
+$(".com_movies").dblclick(function(e){
+	$("#pop-up").removeClass("pop-out");
+	$('#darken').removeClass("pop-out");
+	$('#pop-up').css("z-index","5");
+	$("#pop-up").addClass("pop-in");
+	$('#darken').css("z-index", "4");
+	$('#darken').addClass("pop-in");
+});
+$(".dra_movies").dblclick(function(e){
+	$("#pop-up").removeClass("pop-out");
+	$('#darken').removeClass("pop-out");
+	$('#pop-up').css("z-index","5");
+	$("#pop-up").addClass("pop-in");
+	$('#darken').css("z-index", "4");
+	$('#darken').addClass("pop-in");
+});
+$(".com_movies").dblclick(function(e){
+	$("#pop-up").removeClass("pop-out");
+	$('#darken').removeClass("pop-out");
+	$('#pop-up').css("z-index","5");
+	$("#pop-up").addClass("pop-in");
+	$('#darken').css("z-index", "4");
+	$('#darken').addClass("pop-in");
+});
+
+
+$('#darken').click(function(e){
+	$("#pop-up").removeClass("pop-in");
+	$('#darken').removeClass("pop-in");
+	$("#pop-up").addClass("pop-out");
+	$('#darken').addClass("pop-out");
+	window.setTimeout(function(){
+		$('#darken').css("z-index", "1");
+		$('#pop-up').css("z-index","2");
+	},1000);
 });
